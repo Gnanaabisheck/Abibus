20a9238fd8326b3a60733f720abf000e5b8ae283
Gnanaabisheck <gnanaabisheck@gmail.com>
2023-11-23 21:31:17 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react'
+import Header from './BusBookingSite/Header'
+import {Route, Routes, useNavigate} from 'react-router-dom'
+import Search from './BusBookingSite/Search'
+import { Buses, Cities } from './BusBookingSite/Buses'
+import BusList from './BusBookingSite/BusList'
+import SeatLayout from './BusBookingSite/SeatLayout'
+import BookingPage from './BusBookingSite/BookingPage'
+import Final from './BusBookingSite/Final'
+export const newContext = createContext(null)
+const App = () => {
+  
 
-function App() {
+  const navigate=useNavigate()
+  const [empty,setEmpty]=useState({
+    name:'',
+    email:'',
+    mobno:''
+  })
+ const[selectedSeats,setSelectedSeats]=useState([])
+  const[searchState,setSearchState]=useState(
+    {
+    from:Cities[0],
+    to:Cities[2],
+    date:''
+    }
+  )
+  const[filterdBuses,setFilterdBuses]=useState('')
+    function handleClick(){
+        setFilterdBuses(Buses.filter((bus)=>bus.source===searchState.from && bus.destination===searchState.to && bus.availabledates.includes(searchState.date)))
+          navigate('/buslist')
+    }
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+     <newContext.Provider value={{searchState,setSearchState,filterdBuses,setFilterdBuses,handleClick,selectedSeats,setSelectedSeats,empty,setEmpty}}>
+     <Routes>
+        <Route path='/' element={<Search/>}/>
+        <Route path='/buslist' element={<BusList/>}/>
+        <Route path='/seatlayout/:id' element={<SeatLayout/>}/>
+        <Route path='/bookingpage' element={<BookingPage/>}/>
+        <Route path='/final' element={<Final/>}/>
+
+      </Routes>
+     </newContext.Provider>
+
+    
+     
+       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
